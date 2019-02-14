8fa4ae67175abf2b2df030430eccd86cd33d77e2
cartervh <36041816+cartervh@users.noreply.github.com>
2019-02-13 23:30:13 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Navigation from './component/Navigation/Navigation'
+import Navigation from './components/Navigation/Navigation'
+import Navigation from './components/Logo/Logo'
 class App extends Component {
   render() {
     return (
       <div className="App">
       	<Navigation />
       	{
-      		/*<Logo />
-			<ImageLinkForm />
+      		<Logo />
+			/*<ImageLinkForm />
 			<FaceRecognition />*/
       	}
       </div>
